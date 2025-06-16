fd96eea575afb12f6ecb29f6ba48a40b2ad6f7e0
Isra7u7 <75089765+Isra7u7@users.noreply.github.com>
2025-06-15 18:58:25 -0600

    imagenes

--- src/components/Bienvenida.jsx
+++ src/components/Bienvenida.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 
 const images = [
-  "/img/bienvenida.jpg",
-  "/img/bienvenida2.jpg",
-  "/img/bienvenida4.jpg",
+  "/img/bienvenida11.jpg",
+  "/img/bienvenida10.jpg",
+  "/img/bienvenida5.jpg",
 ];
 
 function WelcomeBanner() {
@@ -19,15 +19,15 @@ function WelcomeBanner() {
 
   return (
   <div className="relative w-full max-w-7xl mx-auto overflow-hidden rounded-lg shadow-lg">
-    <img
-      src={images[currentIndex]}
-      alt="Banner"
-      className="w-full h-[500px] object-cover transition-opacity duration-1000"
-    />
-    <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-0">
-      
-    </div>
+  <img
+    src={images[currentIndex]}
+    alt="Banner"
+    className="w-full h-[300px] sm:h-[400px] md:h-[500px] lg:h-[600px] object-cover transition-opacity duration-1000"
+  />
+  <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-0">
+    
   </div>
+</div>
 );
 }
 
